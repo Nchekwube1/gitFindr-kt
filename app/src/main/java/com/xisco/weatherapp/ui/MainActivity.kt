bb95a5292e360f72f61ddb6f01b9eb24c8b12695
Unekwe Francis <unekwenchekwube@gmail.com>
2022-05-18 17:22:44 +0100

    added folder structure

--- app/src/main/java/com/xisco/weatherapp/ui/MainActivity.kt
+++ app/src/main/java/com/xisco/weatherapp/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.xisco.weatherapp
+package com.xisco.weatherapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.xisco.weatherapp.R
 
 class MainActivity : AppCompatActivity() {
             override fun onCreate(savedInstanceState: Bundle?) {
